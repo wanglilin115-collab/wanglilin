c5d527754f973e6c0310bfcec0b4aa55e3925a5c
wanglilin115-collab <wanglilin115@gmail.com>
2026-03-29 12:35:11 +0800

    增加ex06

---
+++ ex06/ex06.ino
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#define LED_A_PIN 2
+#define LED_B_PIN 13
+
+int pwm_val = 0;
+int step = 2;
+
+void setup() {
+  pinMode(LED_A_PIN, OUTPUT);
+  pinMode(LED_B_PIN, OUTPUT);
+}
+
+void loop() {
+  analogWrite(LED_A_PIN, pwm_val);
+  analogWrite(LED_B_PIN, 255 - pwm_val);
+
+  pwm_val += step;
+  if (pwm_val >= 255 || pwm_val <= 0) {
+    step = -step;
+  }
+  delay(10);
+}
